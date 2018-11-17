1c24e1f46339b5ac71fb3a320ee30880b5c536bb
Andy <40170368+AntekiA@users.noreply.github.com>
2018-11-18 00:18:34 +0900

    Update atoi.py

--- atoi.py
+++ atoi.py
@@ -1,29 +1,30 @@
-import re        
-
-class Solution(object):
+class Solution:
     def myAtoi(self, str):
         """
         :type str: str
         :rtype: int
-        """
-        min = -2147483648        
-        max = 2147483647 
-        chars='+-0123456789'
+        """ 
         str = str.strip()
-        
-        if not re.match(r'^(\-|\+)?\d+', str):
-            return 0   
-        
-        
-        i=1 if str[0] in chars[0:2] else 0
-        
+        if len(str) == 0:
+            return 0
+        min = -2147483648        
+        max = 2147483647
+        chars='+-0123456789'
+        i = 0
+        if str[0] not in chars:
+            return 0
+        if str[0] == '-' or str[0] == '+':
+            i = 1
         while i < len(str) and str[i] in chars[2:]:
-            i+=1
+            i += 1
+        try:
+            res = int(str[:i])
+            if min <= res <= max:
+                return res
+            elif res < min:
+                return min
+            else: 
+                return max
+        except:
+            return 0 
         
-        res = int(str[:i])
-        if min <= res <= max:
-            return res
-        elif res < min:
-            return min
-        else: 
-            return max
