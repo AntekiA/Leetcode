69fa9fbe696903849d82a270bfd37eaf89563877
Andy <40170368+AntekiA@users.noreply.github.com>
2018-11-18 12:51:04 +0900

    Update 15.3Sum.py

--- 15.3Sum.py
+++ 15.3Sum.py
@@ -23,3 +23,36 @@ class Solution:
                                     tri.append(sorted([nums[i], nums[j], nums[u]]))
                                 
         return tri
+
+    
+    class Solution2:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+        dic = {}
+        tri = []
+        nums.sort()
+        st = 0
+        le = len(nums)-1
+        for i in range(le):
+            if nums[i] not in dic:
+                dic[nums[i]] = [i]
+            else:
+                dic[nums[i]].append(i)
+        while st < len(nums)  and nums[st] <= 0:
+            le = len(nums)-1
+            while (le - st) > 1 and nums[le] >= 0:
+                tar = 0-nums[st]-nums[le]
+                if tar in dic:
+                    for u in dic[tar]:
+                        if u != st and u != le :
+                            if sorted([nums[st], nums[le], nums[u]]) not in tri:
+                                tri.append(sorted([nums[st], nums[le], nums[u]]))
+                le -= 1
+            st += 1
+                                
+        return tri
