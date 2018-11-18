c57f86a49e0fed9ac6a5a66e44fb61bff8b99c13
Andy <40170368+AntekiA@users.noreply.github.com>
2018-11-18 16:06:39 +0900

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -27,3 +27,20 @@ class Solution:
             return not stack
         except:
             return bool(0)
+        
+class Solution_better:
+    def isValid(self, s):
+        stack = []
+        dic = {')':'(', ']':'[', '}':'{'}
+        for i, v in enumerate(s):
+            if v in ['(','[','{']:
+                stack.append(v)
+            elif v in dic:
+                if len(stack) > 0 and dic[v] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False # Closing wihle no parenthesis was open
+        if stack != []:
+            return False # Mismatch
+        else:
+            return True
