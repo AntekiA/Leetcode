72971ad40d2f7626080077c915b79db03459a24f
Andy <40170368+AntekiA@users.noreply.github.com>
2018-11-18 23:49:37 +0900

    Update 28. Implement strStr().py

--- 28. Implement strStr().py
+++ 28. Implement strStr().py
@@ -11,3 +11,50 @@ class Solution:
             return haystack.index(needle)
         else:
             return -1
+
+class Solution_KMP(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == '':
+            return 0
+        return self.kmp(haystack,needle)
+        
+    def makeNext(self,P):
+        next = [0] * len(P)
+        
+        q = 1 # cursor in P, starting from 1
+        k = 0 # k is the length of maximum common suffix and prefix
+        
+        while q < len(P):
+            while k > 0 and P[q] != P[k]:
+                k = next[k-1]
+            if P[q] == P[k]:
+                k+=1
+            next[q] = k
+            q +=1
+        
+        return next
+            
+    
+    def kmp(self,T,P):
+        
+        next = self.makeNext(P)
+        
+        i = 0 #cursor in T
+        q = 0 #cursor in P
+        while i < len(T):
+            while q > 0 and P[q] != T[i]:
+                q = next[q-1]
+            
+            if T[i] == P[q]:
+                q += 1
+            
+            if q == len(P):
+                return i - len(P) + 1
+            i+=1
+        
+        return -1
