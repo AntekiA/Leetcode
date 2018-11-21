c3ce10c4649611d6af0007082d733d0ed3b0c16e
Andy <40170368+AntekiA@users.noreply.github.com>
2018-11-21 22:46:14 +0900

    Update 127. Word Ladder.py

--- 127. Word Ladder.py
+++ 127. Word Ladder.py
@@ -1,49 +1,3 @@
-class Solution:
-    def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
-        if len(wordList) == 0:
-            return 0
-        if endWord not in wordList:
-            return 0
-        if not beginWord == wordList[0]:
-            wordList.insert(0, beginWord)
-        i = 0
-        lis = []
-        while i < len(wordList):
-            if self.compare(wordList[i], endWord):
-                if wordList[i] not in lis:
-                    lis.append(wordList[i])
-                lis.append(endWord)
-                break
-            elif self.compare(wordList[i], wordList[i+1]):
-                if wordList[i] not in lis:
-                    lis.append(wordList[i])
-                lis.append(wordList[i+1])
-                i += 1
-            else:
-                break
-        return len(lis)
-
-        
-    def compare(self, worda, wordb):
-        worda = list(worda)
-        wordb = list(wordb)
-        for i in worda:
-            if i in wordb:
-                del wordb[wordb.index(i)]
-        if len(wordb) == 1 or len(wordb) == 0:
-            return True
-        else:
-            return False
-
-from collections import deque
-
-
 from collections import deque
 
 
@@ -81,3 +35,46 @@ class Solution(object):
         d = construct_dict(set(wordList))
         # print(d)
         return bfs_words(beginWord, endWord, d)
+
+#     class Solution:
+#     def ladderLength(self, beginWord, endWord, wordList):
+#         """
+#         :type beginWord: str
+#         :type endWord: str
+#         :type wordList: List[str]
+#         :rtype: int
+#         """
+#         if len(wordList) == 0:
+#             return 0
+#         if endWord not in wordList:
+#             return 0
+#         if not beginWord == wordList[0]:
+#             wordList.insert(0, beginWord)
+#         i = 0
+#         lis = []
+#         while i < len(wordList):
+#             if self.compare(wordList[i], endWord):
+#                 if wordList[i] not in lis:
+#                     lis.append(wordList[i])
+#                 lis.append(endWord)
+#                 break
+#             elif self.compare(wordList[i], wordList[i+1]):
+#                 if wordList[i] not in lis:
+#                     lis.append(wordList[i])
+#                 lis.append(wordList[i+1])
+#                 i += 1
+#             else:
+#                 break
+#         return len(lis)
+
+        
+#     def compare(self, worda, wordb):
+#         worda = list(worda)
+#         wordb = list(wordb)
+#         for i in worda:
+#             if i in wordb:
+#                 del wordb[wordb.index(i)]
+#         if len(wordb) == 1 or len(wordb) == 0:
+#             return True
+#         else:
+#             return False
